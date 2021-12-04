19f54da80b5bb24778e24db10eb234f911887fca
abrar <alisher5591241@gmail.com>
2021-12-04 04:55:35 -0600

    cashregister agency api worked

--- Services/CashRegisterAgency.js
+++ Services/CashRegisterAgency.js
@@ -11,16 +11,17 @@ const catchAsync = require('../utils/catchAsync');
 exports.Add = catchAsync(async (req, res, next) => {
 
     const Record = await CashRegisterAgencyModel.create({ ...req.body })
-    console.log("Record", Record)
+
     if (!Record) {
         throw new Error('Error! CashRegister cannot be created');
     }
     else {
-        const Agency = await AgencyModel.find({ "_id": req.body.Member })
-        console.log("Member Found===", Agency)
+        const Agency = await AgencyModel.find({ "_id": req.body.Agency })
+        console.log("Agency Found===", Agency)
         if (Agency[0]) {
             Agency[0].CashRegister.push(Record._id);
-            const save = await Member[0].save()
+            const save = await Agency[0].save()
+            console.log("save", save)
         }
         return res.status(201).json({
             success: true, message: "CashRegister Created Successfully", Record
@@ -59,18 +60,34 @@ exports.UpdateStatus = catchAsync(async (req, res, next) => {
 
 //Delete
 exports.Delete = catchAsync(async (req, res, next) => {
-
+    console.log("Member Delete hit ",)
     try {
+        const CashRegister = await CashRegisterAgencyModel.find({
+            "_id": req.body.CashRegisterAgency
+        })
+        console.log("CashRegister===>>>", CashRegister)
+        const Agency = await AgencyModel.find({
+            "_id": CashRegister[0].Agency
+        })
+        console.log("Found===>>>", Agency)
+        if (Agency[0]) {
+            const index = await Agency[0].CashRegister.indexOf(req.body.CashRegisterAgency);
+            console.log("index===", index)
+            Agency[0].CashRegister.splice(index, 1)
+            const save = await Agency[0].save()
+        }
 
-        const Record = await CashRegisterAgencyModel.deleteOne({"_id": req.body.CashRegisterAgency})
-        console.log("delete",Record)
+        const Record = await CashRegisterAgencyModel.deleteOne({
+            "_id": req.body.CashRegisterAgency
+        })
+        console.log("delete", Record)
         if (Record.deletedCount > 0) {
             return res.status(200).json({
                 success: true, message: "Cash Register Deleted Successfully"
             })
         }
         return res.status(500).json({
-            success: false, message: "Error!  Cash Register with this ID Not Found"
+            success: false, message: "Error! Cash Register with this ID Not Found"
         })
 
 
